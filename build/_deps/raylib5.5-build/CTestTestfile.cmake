4304a5b71f288997d25211554ccb25335afb34c5
Saleh Ghobari <kreanal355@outlook.com>
2025-05-03 05:19:26 +0300

    resizable window

--- build/_deps/raylib5.5-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Saleh/Desktop/DungeonCrawler/build/_deps/raylib5.5-src
-# Build directory: C:/Users/Saleh/Desktop/DungeonCrawler/build/_deps/raylib5.5-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,24 @@
 #include "raylib.h"
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+#include <iostream>
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
-
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    int TextWidth = MeasureText("Congrats! You created your first window!", 20);
+    SetTargetFPS(60); 
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-            DrawText("bromomento numero dos", 190, 220, 20, LIGHTGRAY);
+            ClearBackground(PURPLE);
+            DrawText("Congrats! You created your first window!", GetScreenWidth() / 2.0 - TextWidth / 2.0, GetScreenHeight() / 2.0 - 10, 20, LIGHTGRAY);
+            std::cout << "Screen Width: " << GetScreenWidth() << " Screen Height: " << GetScreenHeight() << "\n";int MeasureText(const char *text, int fontSize);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+
+    CloseWindow(); 
 
     return 0;
 }
